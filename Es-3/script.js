7e380e30b5a8e9756b3ee2e4705a1849acc97981
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-12 15:22:13 +0200

    es -3

--- Es-3/script.js
+++ Es-3/script.js
@@ -4,7 +4,7 @@ var numeriInseriti = []
 for (var i = 0; i < 10; i++) {
 
     var numero = parseInt(prompt("Inserisci numero. Ne mancano" + (10 - i)));
-
+    
     somma += numero
 
     numeriInseriti.push(numero)
